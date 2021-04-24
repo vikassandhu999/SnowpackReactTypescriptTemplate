e31e9254d4a829ef7cf64f069aaa58fa7ef08920
vikassandhu999 <vikassandhu999@gmail.com>
2021-04-24 15:21:02 +0000

    v.1:template

---
+++ snowpack.config.js
@@ -0,0 +1,42 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-typescript',
+      {
+        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+      },
+    ],
+  ],
+   alias: {
+        "@app": "./src/",
+    },
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    "bundle": true,
+    "splitting":true,
+    "minify":true,
+    "treeshake":true,
+    "target":"es2017"
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+
+  return (
+    <div className="App">
+      This starterpack is made for fast development and optimized build
+    </div>
+  );
+}
+
+export default App;
